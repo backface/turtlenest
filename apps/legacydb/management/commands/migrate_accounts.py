9107bced5b51b2ef9c5abb69bb03684e9a128b39
Michael <m@ash.to>
2024-11-06 20:16:52 +0100

    try new ordering

--- apps/legacydb/management/commands/migrate_accounts.py
+++ apps/legacydb/management/commands/migrate_accounts.py
@@ -30,7 +30,7 @@ def create_groups():
 
 def migrate_userdata():
     print("migrating user data ..", end="\n")
-    oldusers = OldUser.objects.using("legacydb").order_by("joined")
+    oldusers = OldUser.objects.order_by("joined", "id").using("legacydb")
     p = Paginator(oldusers, 100)    
     print(f"  .. total objects: {p.count} ({p.num_pages})..")
     i = 0
@@ -41,59 +41,68 @@ def migrate_userdata():
             progress = (i / p.count) * 100
             print(f"\r  .. {(progress):.1f}% ({i})..", end="", flush=True)
 
-            new_user, created = User.objects.get_or_create(
-                username=person.username,
-                email=person.email if person.email else f"{person.username}@turtlestitch.org"
-            )
+            # new_user, created = User.objects.get_or_create(
+            #     username=person.username,
+            #     email=person.email if person.email else f"{person.username}@turtlestitch.org"
+            # )
             # new_user.email = (
             #     person.email if person.email else f"{person.username}@turtlestitch.org"
             # )
-            new_user.password = "crypt$21$" + person.password
-            new_user.date_joined = person.joined
-            new_user.last_login = person.last_active
-            new_user.about = person.about
-            new_user.location = person.location.replace("Click to edit your location.", "") if person.location else ""
-            new_user.notify_comment = person.notify_comment
-            new_user.notify_like = person.notify_like
-            new_user.verified = person.confirmed
-            new_user.is_mentor = person.is_teacher
-            new_user.save()
 
-            new_account, created = EmailAddress.objects.get_or_create(
-                user=new_user,
-                email=new_user.email
-            )
-            new_account.verified = person.confirmed
-            new_account.primary = True
-            new_account.save()
+            if person.username == "group13tobyh":
+                print(f"\n{person.username}")
+            
+            # # password is hashed in the legacy database, so we need to add 'crypt$21$' prefix for Django to recognize it as a bcrypt hash. 
+            # # This might be different depending on your setup and how you handle passwords in Django.
+            # new_user.password = "crypt$21$" + person.password
+            # new_user.date_joined = person.joined
+            # new_user.last_login = person.last_active
+            # new_user.about = person.about
+            # new_user.location = person.location.replace("Click to edit your location.", "") if person.location else ""
+            # new_user.notify_comment = person.notify_comment
+            # new_user.notify_like = person.notify_like
+            # new_user.verified = person.confirmed
+            # new_user.is_mentor = person.is_teacher
+            # new_user.save()
 
-            if person.has_teacher:
-                try: 
-                    teacher = User.objects.get(username=person.has_teacher)
-                except User.DoesNotExist:
-                    print("Teacher not found", person.has_teacher, "for", person.username)
-                    raise Exception("Teacher not found")
-                else:
-                    new_user.mentor = (
-                        User.objects.get(username=person.has_teacher)
-                        if person.has_teacher
-                        else None
-                    )
-            if person.isadmin:
-                new_user.is_staff = True
-            if person.ismoderator:
-                new_user.groups.add(Group.objects.get(name="Moderators").id)
-            if person.is_teacher:
-                new_user.groups.add(Group.objects.get(name="Teachers").id)
-            if person.is_puppet:
-                new_user.groups.add(Group.objects.get(name="Puppets").id)
+            # new_account, created = EmailAddress.objects.get_or_create(
+            #     user=new_user,
+            #     email=new_user.email
+            # )
+            # new_account.verified = person.confirmed
+            # new_account.primary = True
+            # new_account.save()
 
-            if new_user.username == "mash":
-                new_user.is_superuser = True
-            if new_user.username == "chmod":
-                new_user.is_superuser = True
+            # if person.has_teacher:
+            #     try: 
+            #         teacher = User.objects.get(username=person.has_teacher)
+            #     except User.DoesNotExist:
+            #         print("Teacher not found", person.has_teacher, "for", person.username)
+            #         raise Exception("Teacher not found")
+            #     else:
+            #         new_user.mentor = (
+            #             User.objects.get(username=person.has_teacher)
+            #             if person.has_teacher
+            #             else None
+            #         )
+            # if person.isadmin:
+            #     new_user.is_staff = True
+            # if person.ismoderator:
+            #     new_user.groups.add(Group.objects.get(name="Moderators").id)
+            # if person.is_teacher:
+            #     new_user.groups.add(Group.objects.get(name="Teachers").id)
+            # if person.is_puppet:
+            #     new_user.groups.add(Group.objects.get(name="Puppets").id)
 
-            new_user.save()
-        
+            # if new_user.username == "mash":
+            #     new_user.is_superuser = True
+            # if new_user.username == "chmod":
+            #     new_user.is_superuser = True
+
+            # new_user.save()
+
+
+            # if person.username == "group13tobyh":
+            #     print(f"\n{person.username}")
 
     print("\nDone.")
