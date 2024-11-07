85f0ab2fc6e1ea5160eb5fad0a74571301d7f003
Michael <m@ash.to>
2024-11-07 13:40:44 +0100

    move to sample

--- redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# to be called by a webhook
-# we suppose docker-compose is and alias for either "podman-compose" or "docker compose"
-
-su turtle -c "git pull"
-su turtle -c "docker compose build"
-su turtle -c "docker compose up -d"
